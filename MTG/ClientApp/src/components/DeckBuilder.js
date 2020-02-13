92b69fd937f00b9b2501bdc599321b24b15405dd
Mateus <Mateus@DESKTOP-PBKIP0N>
2020-02-13 16:59:01 -0300

    Setted tiles

--- MTG/ClientApp/src/components/DeckBuilder.js
+++ MTG/ClientApp/src/components/DeckBuilder.js
@@ -13,45 +13,67 @@ export class DeckBuilder extends Component {
                 <div className="deck-builder-container">
                     <div className="card-list-container tile is-ancestor">
                         <div className="tile is-vertical is-4">
-                            <div className="list is-hoverable">
-                                <a className="list-item">
-                                    Featured
+                            <div className="tile is-parent">
+                                <div className="list is-hoverable">
+                                    <a className="list-item">
+                                        Featured
       </a>
-                                <a className="list-item">
-                                    All Posts
+                                    <a className="list-item">
+                                        All Posts
       </a>
-                                <a className="list-item is-active">
-                                    Announcements
+                                    <a className="list-item is-active">
+                                        Announcements
       </a>
-                                <a className="list-item">
-                                    Business
+                                    <a className="list-item">
+                                        Business
       </a>
-                                <a className="list-item">
-                                    Community
+                                    <a className="list-item">
+                                        Community
       </a>
-                                <a className="list-item">
-                                    Data
+                                    <a className="list-item">
+                                        Data
       </a>
-                                <a className="list-item">
-                                    Editor Tools
+                                    <a className="list-item">
+                                        Editor Tools
       </a>
-                                <a className="list-item">
-                                    Education
+                                    <a className="list-item">
+                                        Education
       </a>
-                                <a className="list-item">
-                                    Engineering
+                                    <a className="list-item">
+                                        Engineering
       </a>
-                                <a className="list-item">
-                                    Events
+                                    <a className="list-item">
+                                        Events
       </a>
-                                <a className="list-item">
-                                    Policy
+                                    <a className="list-item">
+                                        Policy
       </a>
-                            </div>                            
+                                </div>
+                            </div>
                         </div>
-                        <div></div>
-                        
-                        
+                        <div className="tile is-vertical is-parent  is-4">
+                            <div className="tile is-child">
+                                <article class="tile is-child notification is-primary">
+                                    <p class="title">Vertical...</p>
+                                    <p class="subtitle">Top tile</p>
+                                </article>
+                            </div>
+                        </div>
+                        <div className="tile is-vertical is-parent is-4">
+                            <div className="tile is-child">
+                                <article class="tile is-child notification is-primary">
+                                    <p class="title">Vertical...</p>
+                                    <p class="subtitle">Top tile</p>
+                                </article>
+                            </div>
+                            <div className="tile is-child">
+                                <article class="tile is-child notification is-primary">
+                                    <p class="title">Vertical...</p>
+                                    <p class="subtitle">Top tile</p>
+                                </article>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
